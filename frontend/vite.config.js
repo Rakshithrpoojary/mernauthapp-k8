6b8931160421da8d8846ff00a3100d4018a7f1ce
Rakshith <poojaryrakshith986@gmail.com>
2026-01-26 15:18:10 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+console.log(process.env.VITE_API_URL);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
