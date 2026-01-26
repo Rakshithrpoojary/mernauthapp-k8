48063d1589ad54c2c5889d6303d2d0100a20f6ad
Rakshith <poojaryrakshith986@gmail.com>
2026-01-26 17:33:38 +0530

    first commit

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,21 +1,22 @@
-import { Container, Card, Button } from 'react-bootstrap';
+import { Container, Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className=' py-5'>
-      <Container className='d-flex justify-content-center'>
-        <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
-          <h1 className='text-center mb-4'>MERN Authentication</h1>
-          <p className='text-center mb-4'>
+    <div className=" py-5">
+      <Container className="d-flex justify-content-center">
+        <Card className="p-5 d-flex flex-column align-items-center hero-card bg-light w-75">
+          <h1 className="text-center mb-4">MERN Authentication</h1>
+          <p className="text-center mb-4">
             This is a boilerplate for MERN authentication that stores a JWT in
             an HTTP-Only cookie. It also uses Redux Toolkit and the React
             Bootstrap library
           </p>
-          <div className='d-flex'>
-            <Button variant='primary' href='/login' className='me-3'>
-              Sign In
+          <div className="d-flex">
+            <Button variant="primary" className="me-3">
+              <Link to="/login">Sign In</Link>
             </Button>
-            <Button variant='secondary' href='/register'>
+            <Button variant="secondary" href="/register">
               Register
             </Button>
           </div>
