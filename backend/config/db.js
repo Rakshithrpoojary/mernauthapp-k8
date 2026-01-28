8fab954a18f2f57909478c718a3bb92e57a4e3b5
Rakshith <poojaryrakshith986@gmail.com>
2026-01-28 16:24:10 +0530

    jenkins file for frontend

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     // Use MONGO_URI from environment variables
-    console.log("URL",process.env.MONGO_URI);
-    const conn = await mongoose.connect("mongodb://admin:admin123@mongodb-svc:27017/ecommerce?authSource=admin");
+    // console.log("URL",process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb://admin:admin123@mongodb-svc:27017/admin");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
